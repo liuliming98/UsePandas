d66771895f48192e21955f326e31d47c13f61689
liuliming <758185271@qq.com>
2018-10-18 16:35:07 +0800

    test
    First commit

---
+++ TestReadXlsx.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import pandas as pd
+
+
+
+
+def main():
+    data = pd.read_excel("C:\\Users\\TuhuaG\\Desktop\\testpandas\\temp.xlsx")
+    print(data)
+
+if __name__ == '__main__':
+    main()
